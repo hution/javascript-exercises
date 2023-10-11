f29aadb5bcdb85edd6a8c207de7df53f61c389d9
Travis Hawk <hawk.tr@gmail.com>
2023-10-10 22:08:36 -0700

    Finished first 3 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, val,...moreVal) {
+            
+        for(let i = 0;i < array.length;i++){
+            let j = 0;
+            
 
+            while(j<moreVal.length){
+                if (array[i]== moreVal[j] && typeof array[i] == typeof moreVal[j]){
+                    array.splice(i,1);
+                }
+                j++;
+            }
+
+            if (array[i]== val && typeof array[i] == typeof val){
+                array.splice(i,1);
+                i--;
+            }
+        }
+    
+    return array;
 };
 
+console.log(removeFromArray([1, 2, 3], "1", 3))
 // Do not edit below this line
 module.exports = removeFromArray;
