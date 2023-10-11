c566dd19ca628d13d47588f33f7a63b5d05e961f
Travis Hawk <hawk.tr@gmail.com>
2023-10-10 22:09:43 -0700

    Added the other missing compelted exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = '';
+    let i = 0;
+    if (num < 0){
+        return "ERROR";
+    }
 
+    while (i < num){
+        newString += string;
+
+        i++;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let tempString = '';
+    let currentPos = string.length;
+    for(let i = 0; i < string.length; i++){
+
+        tempString += string[currentPos-1];
+        currentPos--;
+        // console.log(tempString);
+    }
+    return tempString
+    
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
