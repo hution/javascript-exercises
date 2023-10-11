159e172362d45dded336c41e0392e5b0c94521fd
Travis Hawk <hawk.tr@gmail.com>
2023-10-10 23:02:06 -0700

    Finished the remaining JS exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }else if(typeof num1 != "number" || typeof num2 != "number")
+    {
+        return "ERROR";
+    }
 
+    if (num1 > num2){
+        let tempNum = num2;
+        num2 = num1;
+        num1 = tempNum;
+    }
+
+
+    for(let i = num1; i <= num2; i++){
+        totalSum = totalSum+i;
+    }
+    return totalSum;
 };
 
+console.log(sumAll(123, 1))
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  return Math.round(((val-32) * (5/9))*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val) {
+  return Math.round(((val * (9/5) + 32)) * 10)/10;
 };
+
+console.log(convertToCelsius(100)) // fahrenheit to celsius, should return 0
+
+console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
