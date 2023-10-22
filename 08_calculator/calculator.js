0598f4b15002152d86a39522bc64a2bb9d760304
Travis Hawk <hawk.tr@gmail.com>
2023-10-22 11:36:14 -0700

    Finished 08 calcalculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(vals) {
+	let sum = 0;
+  vals.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(vals) {
+  let product = 1;
+  vals.forEach((element) => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	if (val == 0){
+    return 1;
+  }
+  else {
+    return (val * factorial(val-1));
+  }
 };
 
 // Do not edit below this line
