edca89a5cf5a7f758f8cea858bbcbc03faf0092f
Travis Hawk <hawk.tr@gmail.com>
2023-10-23 22:56:15 -0700

    Finished exercise number 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    let oldest = 0;
+    const ordered = list.sort((a,b) => ((a.yearOfDeath || 2023) - a.yearOfBirth) < ((b.yearOfDeath || 2023) - b.yearOfBirth) ? 1 : -1);
+    return ordered[0]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
